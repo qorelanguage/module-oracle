95793961926ac8db9f0fe081ac6927356d34169b
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2012-12-31 12:41:47 +0000

    added a custom deref() function for AQMessagePriv to manage references
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/module-oracle/trunk@5336 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- src/AQMessagePriv.h
+++ src/AQMessagePriv.h
@@ -1,3 +1,5 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*- */
+
 #ifndef AQMESSAGEPRIV_H
 #define AQMESSAGEPRIV_H
 
@@ -5,53 +7,59 @@
 #include "oracle.h"
 #include "ocilib_internal.h"
 
-
 class AQQueuePriv;
-
 
 DLLEXPORT extern qore_classid_t CID_AQMESSAGE;
 DLLLOCAL extern QoreClass* QC_AQMESSAGE;
 
-
 class AQMessagePriv : public AbstractPrivateData {
-   protected:
-      DLLLOCAL virtual ~AQMessagePriv();
+protected:
+   DLLLOCAL virtual ~AQMessagePriv();
 
-   public:
-      DLLLOCAL AQMessagePriv(const QoreHashNode* obj = 0);
+public:
+   DLLLOCAL AQMessagePriv(const QoreHashNode* obj = 0);
 
-      DLLLOCAL QoreHashNode* getObject();
-      DLLLOCAL void setObject(const QoreHashNode* obj=0);
+   DLLLOCAL QoreHashNode* getObject();
+   DLLLOCAL void setObject(const QoreHashNode* obj=0);
 
-      DLLLOCAL int64 getAttemptCount();
+   DLLLOCAL int64 getAttemptCount();
 
-      DLLLOCAL int64 getEnqueueDelay();
-      DLLLOCAL void setEnqueueDelay(int64 delay);
+   DLLLOCAL int64 getEnqueueDelay();
+   DLLLOCAL void setEnqueueDelay(int64 delay);
 
 //      OCI_Date* getEnqueueTime();
 
-      DLLLOCAL int64 getExpiration();
-      DLLLOCAL void setExpiration(int64 expiration );
+   DLLLOCAL int64 getExpiration();
+   DLLLOCAL void setExpiration(int64 expiration );
 
-      DLLLOCAL int64 getState();
+   DLLLOCAL int64 getState();
 
-      DLLLOCAL int getPriority();
-      DLLLOCAL void setPriority(int64 prio);
+   DLLLOCAL int getPriority();
+   DLLLOCAL void setPriority(int64 prio);
 
 //      DLLLOCAL void getID(void *id, unsigned int *len);
 
-      DLLLOCAL QoreStringNode* getCorrelation();
-      DLLLOCAL void setCorrelation(const QoreStringNode* c);
+   DLLLOCAL QoreStringNode* getCorrelation();
+   DLLLOCAL void setCorrelation(const QoreStringNode* c);
 
+   DLLLOCAL virtual void deref(ExceptionSink* xsink) {
+      if (ROdereference()) {
+	 if (m_obj)
+	    m_obj->deref(xsink);
+	 if (m_correlation)
+	    m_correlation->deref();
+	 delete this;
+      }
+   }
 
-   private:
-      QoreHashNode *m_obj;
-      int64 m_attemptCount;
-      int64 m_enqueueDelay;
-      int64 m_expiration;
-      int64 m_state;
-      int64 m_priority;
-      QoreStringNode *m_correlation;
+private:
+   QoreHashNode *m_obj;
+   int64 m_attemptCount;
+   int64 m_enqueueDelay;
+   int64 m_expiration;
+   int64 m_state;
+   int64 m_priority;
+   QoreStringNode *m_correlation;
 };
 
 #endif
